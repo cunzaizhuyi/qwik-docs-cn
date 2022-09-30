090fb85353f30662ef002128422e19530200d273
cunzaizhuyi <877824709@qq.com>
2022-09-30 21:32:11 +0800

    feat: 初始化项目

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vitepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# just a example, please change the follow line with your github account!!!
+ git push -f https://github.com/cunzaizhuyi/cunzaizhuyi.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
